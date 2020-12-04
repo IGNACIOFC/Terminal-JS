bb20563d08dde35567396eaaa03d1caab4179181
Andres Auais <andresauais@gmail.com>
2020-12-04 17:15:00 +0000

    main function

---
+++ data.js
@@ -0,0 +1,15 @@
+localStorage.setItem("arr", JSON.stringify([
+["user", []],
+["desktop", [
+    ["projects",[]],
+    ["assembler", []],
+    ["documents", [
+        {name: "Homework.doc",
+        content: "Ex 1",},
+        {name: "Practial.js",
+        content: "3+2"},
+        ["pills", []]
+    ]]
+]],
+["downloads", []]
+]));
--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function init(){
     var terminalInit = create("div");
     var initText = create("p");
     initText.innerHTML = ">Welcome to our terminal";
+
     terminalInit.append(initText);
     terminalInit.append(createInput());
     terminal.append(terminalInit);
@@ -31,15 +32,85 @@ function submitInput(event){
 }
 
 function mainEvent(inputValue){
-    switch(inputValue){
-        case "ls":
-            alert("funcion ls");
-            break;
-        case "cd":
-            alert("funcion cd");
-            break;
+    switch(inputValue.split(" ")[0]){
         case "pwd":
             alert("funcion pwd");
+            break;
+        case "ls":
+            if(inputValue.trim() == "ls"){
+                alert("funcion ls");
+            }
+            else{
+                switch(inputValue.split(" ")[1]){
+                    case "-R":
+                        alert("funcion ls -R");
+                        break;
+                    case "-S":
+                        alert("funcion ls -S");
+                        break;
+                    case "-t":
+                        alert("funcion ls -t");
+                        break;
+                    default:
+                        alert("error");
+                        break;
+                }
+            }
+            break;
+        case "cd":
+            switch(inputValue.split(" ")[1]){
+                case "..":{
+                    alert("funcion cd ..");
+                    break;
+                }
+                default:
+                    if (inputValue.split(" ").length<=2){
+                        alert(inputValue.split(" ")[1]);
+                        break;
+                    }
+                    else{
+                        alert("error");
+                        break;
+                    }
+            }
+            break;
+        case "mkdir":
+            if (inputValue.split(" ").length<=2){
+                alert(inputValue.split(" ")[1]);
+                break;
+            }
+            else{
+                alert("error");
+                break;
+            }
+        case "cat":
+            if (inputValue.split(" ").length<=2){
+                alert(inputValue.split(" ")[1]);
+                break;
+            }
+            else{
+                alert("error");
+                break;
+            }
+        case "rm":
+            if (inputValue.split(" ").length<=2){
+                alert(inputValue.split(" ")[1]);
+                break;
+            }
+            else{
+                alert("error");
+                break;
+            }
+        case "echo":
+        case "mv":
+        case "clear":
+            alert("funcion clear");
+            break;
+        case "help":
+            alert("funcion clear");
+            break;
+        default:
+            alert("error no existe");
             break;
     }
 }
