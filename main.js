a167d5a6070ce032cdc234842033bacc7f42299e
IGNACIOFC <45830117+IGNACIOFC@users.noreply.github.com>
2020-12-09 09:17:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ window.onload = function() {
 };
 
 function init(){
-    var terminalInit = create("div");
+var terminalInit = create("div");
     var initText = create("p");
     initText.innerHTML = ">Welcome to our terminal";
-
     terminalInit.append(initText);
     terminalInit.append(createInput());
     terminal.append(terminalInit);
@@ -32,85 +31,15 @@ function submitInput(event){
 }
 
 function mainEvent(inputValue){
-    switch(inputValue.split(" ")[0]){
-        case "pwd":
-            alert("funcion pwd");
-            break;
+    switch(inputValue){
         case "ls":
-            if(inputValue.trim() == "ls"){
-                alert("funcion ls");
-            }
-            else{
-                switch(inputValue.split(" ")[1]){
-                    case "-R":
-                        alert("funcion ls -R");
-                        break;
-                    case "-S":
-                        alert("funcion ls -S");
-                        break;
-                    case "-t":
-                        alert("funcion ls -t");
-                        break;
-                    default:
-                        alert("error");
-                        break;
-                }
-            }
+            alert("funcion ls");
             break;
         case "cd":
-            switch(inputValue.split(" ")[1]){
-                case "..":{
-                    alert("funcion cd ..");
-                    break;
-                }
-                default:
-                    if (inputValue.split(" ").length<=2){
-                        alert(inputValue.split(" ")[1]);
-                        break;
-                    }
-                    else{
-                        alert("error");
-                        break;
-                    }
-            }
+            alert("funcion cd");
             break;
-        case "mkdir":
-            if (inputValue.split(" ").length<=2){
-                alert(inputValue.split(" ")[1]);
-                break;
-            }
-            else{
-                alert("error");
-                break;
-            }
-        case "cat":
-            if (inputValue.split(" ").length<=2){
-                alert(inputValue.split(" ")[1]);
-                break;
-            }
-            else{
-                alert("error");
-                break;
-            }
-        case "rm":
-            if (inputValue.split(" ").length<=2){
-                alert(inputValue.split(" ")[1]);
-                break;
-            }
-            else{
-                alert("error");
-                break;
-            }
-        case "echo":
-        case "mv":
-        case "clear":
-            alert("funcion clear");
-            break;
-        case "help":
-            alert("funcion clear");
-            break;
-        default:
-            alert("error no existe");
+        case "pwd":
+            alert("funcion pwd");
             break;
     }
 }
